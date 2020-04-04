0407f3b9ee0263bcd8bdbd4dd3587926a5615316
Klemen Berkovic <klemen@MkS2ExE.t-2.net>
2020-04-05 00:54:50 +0200

    Added new starup fixes

--- NiaOrgImage/startup.sh
+++ NiaOrgImage/startup.sh
@@ -5,15 +5,18 @@ NIA_GIT_BRANCH=$2
 NIA_GIT_EXAMPLES=$3
 NIA_GIT_BRANCH_EXAMPLES=$4
 
+curr=$(pwd)
+
 if [ ! -d NiaPy ]; then
 	git clone $NIA_GIT -b $NIA_GIT_BRANCH NiaPy \
 		&& cd NiaPy \
-		&& make build \
-		&& cd ..
+		&& make build
+	cd ${curr}
 fi
 
 if [ ! -d NiaPy-examples ]; then
 	git clone $NIA_GIT_EXAMPLES -b $NIA_GIT_BRANCH_EXAMPLES NiaPy-examples
+	cd ${curr}
 fi
 
 cd NiaOrg-examples && lab
