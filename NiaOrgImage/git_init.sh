39b2ce0bce10c4220d55cd213112389ad459cea5
Klemen Berkovic <klemen@MkS2ExE.t-2.net>
2020-04-05 02:16:32 +0200

    Added new fixes

--- NiaOrgImage/git_init.sh
+++ NiaOrgImage/git_init.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-NIA_GIT=$1
-NIA_GIT_BRANCH=$2
-NIA_GIT_EXAMPLES=$3
-NIA_GIT_BRANCH_EXAMPLES=$4
 
-curr=$(pwd)
+
+cd NiaOrg
+
+curr_wd=$(pwd)
 
 if [ ! -d NiaPy ]; then
 	git clone $NIA_GIT -b $NIA_GIT_BRANCH NiaPy \
 		&& cd NiaPy \
 		&& make build
-	cd ${curr}
+	cd ${curr_wd}
 fi
 
 if [ ! -d NiaPy-examples ]; then
 	git clone $NIA_GIT_EXAMPLES -b $NIA_GIT_BRANCH_EXAMPLES NiaPy-examples
-	cd ${curr}
+	cd ${curr_wd}
 fi
 
 cd NiaOrg-examples && lab
